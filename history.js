051a7b11ccb5f892de23eb165454af04493e657e
amJayem <csejayem19@gmail.com>
2022-09-15 23:27:46 +0600

    padding added to qstn section and history resposive done

--- history.js
+++ history.js
@@ -27,14 +27,39 @@ function displayHistory() {
 
   previousTests.forEach((test) => {
     const newRow = document.createElement("div");
-    newRow.classList.add("card");
+    newRow.classList.add("card1");
+    newRow.classList.add("col-lg-4");
 
     newRow.innerHTML = `
-  <h3>${test.questionText}</h3>
+  <h3 class="text-center">${test.questionText}</h3>
   <p>You took: <span class="bold">${test.timeTaken}</span> seconds</p>
     <p>You made <span class="bold red">${test.errorCount}</span> mistakes</p>
   `;
-
     histories.appendChild(newRow);
   });
 }
+
+/*
+<div class="container text-center">
+  <div class="row">
+    <div class="col">
+      1 of 2
+    </div>
+    <div class="col">
+      2 of 2
+    </div>
+  </div>
+  <div class="row">
+    <div class="col">
+      1 of 3
+    </div>
+    <div class="col">
+      2 of 3
+    </div>
+    <div class="col">
+      3 of 3
+    </div>
+  </div>
+</div>
+
+*/
